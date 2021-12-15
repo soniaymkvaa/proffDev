d53a38078a8f47d5537735e87cb9eeb0f855d53a
soniaymkvaa <96056537+soniaymkvaa@users.noreply.github.com>
2021-12-15 13:18:23 +0200

    added documentation

--- todo.py
+++ todo.py
@@ -3,6 +3,9 @@ import csv
 import time
 from datetime import datetime
 def open_csv_calendar(filepath):
+    """
+    open csv file and read notes from it
+    """
     calendar_notes = []
     with open(filepath,'r') as csvfile:
         calendar = csv.reader(csvfile, delimiter=',', quotechar='|')
@@ -11,6 +14,9 @@ def open_csv_calendar(filepath):
     return calendar_notes
 
 def create_note(name, need_list,deadline,file):
+    """
+    create new note
+    """
     note = [str(datetime.now()),name,need_list,deadline,str(1)]
     calendar_notes = open_csv_calendar(file)
     today = check_day(note,calendar_notes)
@@ -23,16 +29,25 @@ def create_note(name, need_list,deadline,file):
             csvfile.write(','.join(task) + '\n')
 
 def show_notes(file):
+    """
+    print each line in note
+    """
     calendar_notes = open_csv_calendar(file)
     for note in calendar_notes:
         print(f"date: {note[0]} ]name: {note[1]} need: {note[2]} deadline: {note[3]} priority: {note[4]}\n")
 
 def ask_priority():
+    """
+    ask priority to the task
+    """
         print("Give a priority to this task")
         priority = input()
         return priority
 
 def check_day(note:list,calendar_notes):
+    """
+    check if you have othes notes with same deadline
+    """
     deadlines = []
     for each in calendar_notes:
         if str(each[3].split()[0]) == str(note[3].split()[0]):
@@ -41,6 +56,9 @@ def check_day(note:list,calendar_notes):
     return True if len(deadlines) == 0 else False
 
 def show_deadlines(note:list):
+    """
+    print all deadlines
+    """
     deadlines = []
     for each in note:
         if each[3].split()[0] == str(datetime.now()).split()[0]:
@@ -48,10 +66,6 @@ def show_deadlines(note:list):
     deadlines.sort(key=lambda lst: lst[4])
     for each in deadlines:
         print(f'The {each[1]} event with priority {each[4]} is assigned on today. The needlist is: {each[2]}')    
-# def ask_priority():
-
-# def delete_note(name,file):
-
 
 
 if __name__ == "__main__":
