b910d08a971ef4897b669f1aad445cc16923cade
codefloww <kryvpaul@gmail.com>
2021-12-15 12:50:59 +0200

    demo of program

--- todo.py
+++ todo.py
@@ -12,11 +12,11 @@ def open_csv_calendar(filepath):
 
 def create_note(name, need_list,deadline,file):
     note = [str(datetime.now()),name,need_list,deadline,str(1)]
-    today = check_day(note)
+    calendar_notes = open_csv_calendar(file)
+    today = check_day(note,calendar_notes)
     if today:
         priority = ask_priority()
         note[4] = str(priority)
-    calendar_notes = open_csv_calendar(file)
     calendar_notes.append(note)
     with open(file,'w') as csvfile:
         for task in calendar_notes:
@@ -26,11 +26,11 @@ def ask_priority():
         print("Give a priority to this task")
         priority = input()
         return priority
-    
-def check_day(note:list):
+
+def check_day(note:list,calendar_notes):
     deadlines = []
-    for each in note:
-        if each[3].split()[0] == str(datetime.now()).split()[0]:
+    for each in calendar_notes:
+        if str(each[3].split()[0]) == str(note[3].split()[0]):
             deadlines.append(each)
     deadlines.sort(key=lambda lst: lst[4])
     return True if len(deadlines) == 0 else False
@@ -44,8 +44,10 @@ def show_deadlines(note:list):
     for each in deadlines:
         print(f'The {each[1]} event with priority {each[4]} is assigned on today. The needlist is: {each[2]}')    
 # def ask_priority():
-#     pass
-# def check_day(note):
-#     return False
+
+# def delete_note(name,file):
+
+
+
 if __name__ == "__main__":
     create_note('new one', 'nothing', '2021-12-20 12:42:46.433451','calendar.csv')
--- todolist.py
+++ todolist.py
@@ -7,11 +7,11 @@ def main():
         command = input()
         command = command.split(' ')
         if command[0] == 'create':
-            todo.create_note(*command[1:], 'calendar.csv')     
-    def ask_priority():
-        print("Give a priority to this task")
-        priority = input()
-        return priority
+            todo.create_note(*command[1:3],' '.join(command[3:5]), 'calendar.csv')     
+    # def ask_priority():
+    #     print("Give a priority to this task")
+    #     priority = input()
+    #     return priority
 
 if __name__ == "__main__":
     main()
