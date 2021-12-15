7ceaf7c45eae50feba2ab3fed8a6a42589b01462
Paul <90351072+codefloww@users.noreply.github.com>
2021-12-15 13:49:11 +0200

    fixed pylint

--- todolist.py
+++ todolist.py
@@ -1,17 +1,19 @@
 import todo
 
-def main()->None:
+
+def main() -> None:
     """handles main loop and general work and input in terminal"""
     running = True
     while running:
         command = input()
-        command = command.split(' ')
-        if command[0] == 'create':
-            todo.create_note(*command[1:3],' '.join(command[3:5]), 'calendar.csv')  
+        command = command.split(" ")
+        if command[0] == "create":
+            todo.create_note(*command[1:3], " ".join(command[3:5]), "calendar.csv")
         elif command[0] == "show":
-            todo.show_notes('calendar.csv') 
+            todo.show_notes("calendar.csv")
         elif command[0] == "exit":
             quit()
+
 
 if __name__ == "__main__":
     main()
